1509bca23d64acc46cc69c516f0961c0847b1a01
marlen-alishev <marlen.alishev@gmail.com>
2023-07-17 01:54:02 +0600

    Condition is used

---
+++ ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Your name: ");
+string username = Console.ReadLine(); 
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Hey, Mashallah!!!");
+}
+else 
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
